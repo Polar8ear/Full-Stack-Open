e47f0464c9453e9e58022deb9242f40a0b8c9e02
Polar8ear <64390556+Polar8ear@users.noreply.github.com>
2021-06-26 15:41:17 +0800

    Part 1.8 & 1.9

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -6,6 +6,24 @@ const Button = ({text,handleClick}) =>{
   return(<button onClick={handleClick}>{text}</button>)
 }
 
+const Statistics = ({good,neutral,bad})=> {
+  const all=good+neutral+bad
+  
+  if(all!==0){
+    return(
+      <div>
+        <Header text='Statistics'></Header>
+        <div>good {good}</div>
+        <div>neutral {neutral}</div>
+        <div>bad {bad}</div>
+        <div>all {all}</div>
+        <div>average {(good-bad)/all}</div>
+        <div>positive {good/all*100}%</div>
+      </div>
+    )
+  }
+  return (<div>No feedback given</div>)
+}
 
 const App = () => {
   // save clicks of each button to its own state
@@ -13,7 +31,7 @@ const App = () => {
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
 
-  const all=good+neutral+bad
+
 
   return (
     <div>
@@ -22,13 +40,7 @@ const App = () => {
       <Button text="neutral" handleClick={() => setNeutral(neutral+1)}></Button>
       <Button text="bad" handleClick={() => setBad(bad+1)}></Button>
       
-      <Header text='Statistics'></Header>
-      <div>good {good}</div>
-      <div>neutral {neutral}</div>
-      <div>bad {bad}</div>
-      <div>all {all}</div>
-      <div>average {(good-bad)/all}</div>
-      <div>positive {good/all*100}%</div>
+     <Statistics good={good} neutral={neutral} bad={bad}></Statistics>
       
 
     </div>
