36668cc545eee2676ead47696dba1fca02c7e30b
Polar8ear <64390556+Polar8ear@users.noreply.github.com>
2021-06-30 22:36:21 +0800

    Part 2.9*

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -2,18 +2,21 @@ import React, { useState } from 'react'
 
 const App = () => {
   const [ persons, setPersons ] = useState([
-    { name: 'Arto Hellas' ,
-      number: '040-1234567'
-    }
+    { name: 'Arto Hellas', number: '040-123456' },
+    { name: 'Ada Lovelace', number: '39-44-5323523' },
+    { name: 'Dan Abramov', number: '12-43-234345' },
+    { name: 'Mary Poppendieck', number: '39-23-6423122' }
   ]) 
   const [ newName, setNewName ] = useState('')
   const [ newNumber, setNewNumber ] = useState('')
+  const [filter, setFilter] = useState('');
 
   const addNewPerson = (event) =>{
     event.preventDefault()
     if(!persons.some((person)=>newName===person.name)){
       setPersons(persons.concat({name:newName,number:newNumber}))
-      setNewName("");setNewNumber("")
+      setNewName("")
+      setNewNumber("")
     }
     else{
       window.alert(`${newName} is already added to the phonebook`)
@@ -28,9 +31,25 @@ const App = () => {
     )
   }
 
+  const personsToShow = !filter
+    ? persons
+    : persons.filter((person)=>{
+          return(
+            person.name.toUpperCase().includes(filter.toUpperCase())
+          )
+        }
+      )
+      
+
   return (
     <div>
       <h2>Phonebook</h2>
+
+      <form>
+        <div>
+            filter: <input value={filter} onChange={(event)=>setFilter(event.target.value)} />
+        </div>
+      </form>
 
       <form onSubmit={addNewPerson}>
         <div>
@@ -46,7 +65,8 @@ const App = () => {
       </form>
 
       <h2>Numbers</h2>
-      {persons.map(showDetails)}
+      {console.log(personsToShow)}
+      {personsToShow.map(showDetails)}
       
     </div>
   )
