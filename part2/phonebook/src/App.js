068c667c67bf6cac7757b63b565a266ebb414ebc
Polar8ear <64390556+Polar8ear@users.noreply.github.com>
2021-07-01 12:07:06 +0800

    Part 2.10

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,15 +1,16 @@
-import React, { useState } from 'react'
+import React, { Component, useState } from 'react'
 
-const App = () => {
-  const [ persons, setPersons ] = useState([
-    { name: 'Arto Hellas', number: '040-123456' },
-    { name: 'Ada Lovelace', number: '39-44-5323523' },
-    { name: 'Dan Abramov', number: '12-43-234345' },
-    { name: 'Mary Poppendieck', number: '39-23-6423122' }
-  ]) 
-  const [ newName, setNewName ] = useState('')
-  const [ newNumber, setNewNumber ] = useState('')
-  const [filter, setFilter] = useState('');
+const Filter = ({filter,setFilter}) => {
+  return(
+    <form>
+        <div>
+            filter: <input value={filter} onChange={(event)=>setFilter(event.target.value)} />
+        </div>
+      </form>
+  )
+}
+
+const PersonsForm = ({persons,setPersons,newName,setNewName,newNumber,setNewNumber}) =>{ 
 
   const addNewPerson = (event) =>{
     event.preventDefault()
@@ -23,38 +24,15 @@ const App = () => {
     }
   } 
 
-  const showDetails = (person) => {
-    return(
-      <div key={person.name}>
-        {person.name} {person.number}
-      </div>
-    )
-  }
-
-  const personsToShow = !filter
-    ? persons
-    : persons.filter((person)=>{
-          return(
-            person.name.toUpperCase().includes(filter.toUpperCase())
-          )
-        }
-      )
-      
-
-  return (
+  return(
     <div>
-      <h2>Phonebook</h2>
-
-      <form>
-        <div>
-            filter: <input value={filter} onChange={(event)=>setFilter(event.target.value)} />
-        </div>
-      </form>
+      <h3>Add a new</h3>
 
       <form onSubmit={addNewPerson}>
         <div>
           name: <input value={newName} onChange={(event)=>setNewName(event.target.value)} />
         </div>
+
         <div>
           number: <input value={newNumber} onChange={(event)=>setNewNumber(event.target.value)} />
         </div>
@@ -63,10 +41,59 @@ const App = () => {
           <button type="submit">add</button>
         </div>
       </form>
+    </div>
+  )
+}
 
-      <h2>Numbers</h2>
-      {console.log(personsToShow)}
+const Details = ({persons,filter}) =>{
+  const personsToShow = !filter
+  ? persons
+  : persons.filter((person)=>{
+  return(
+          person.name.toUpperCase().includes(filter.toUpperCase())
+          )
+        }
+      )
+  
+  const showDetails = (person) => {
+    return(
+      <div key={person.name}>
+        {person.name} {person.number}
+      </div>
+    )
+  }
+
+  return(
+    <div>
+      <h3>Number</h3>
       {personsToShow.map(showDetails)}
+    </div>
+  )
+}
+
+
+const App = () => {
+  const [ persons, setPersons ] = useState([
+    { name: 'Arto Hellas', number: '040-123456' },
+    { name: 'Ada Lovelace', number: '39-44-5323523' },
+    { name: 'Dan Abramov', number: '12-43-234345' },
+    { name: 'Mary Poppendieck', number: '39-23-6423122' }
+  ]) 
+  const [ newName, setNewName ] = useState('')
+  const [ newNumber, setNewNumber ] = useState('')
+  const [filter, setFilter] = useState('');
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+
+      <Filter filter={filter} setFilter={setFilter}/>
+
+      <PersonsForm persons={persons} setPersons={setPersons} 
+               newName={newName} setNewName={setNewName} 
+               newNumber={newNumber} setNewNumber={setNewNumber}/>
+
+      <Details persons={persons} filter={filter}/>
       
     </div>
   )
