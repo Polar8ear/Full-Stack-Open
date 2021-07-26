8abdf7f2718ae9ba10c83218ad7b4911510bb614
Polar8ear <64390556+Polar8ear@users.noreply.github.com>
2021-07-27 07:14:50 +0800

    Implement blog router controller

--- Part 4/Blogs/index.js
+++ Part 4/Blogs/index.js
@@ -3,6 +3,8 @@ const express = require('express')
 const app = express()
 const cors = require('cors')
 
+const blogsRouter = require('./controllers/blogs')
+
 const Blog = require('./models/blog')
 
 const logger = require('./utils/logger')
@@ -13,23 +15,7 @@ const config = require('./utils/config')
 app.use(cors())
 app.use(express.json())
 
-app.get('/api/blogs', (request, response) => {
-  Blog
-    .find({})
-    .then(blogs => {
-      response.json(blogs)
-    })
-})
-
-app.post('/api/blogs', (request, response) => {
-  const blog = new Blog(request.body)
-
-  blog
-    .save()
-    .then(result => {
-      response.status(201).json(result)
-    })
-})
+app.use('/api/blogs',blogsRouter)
 
 const PORT = config.PORT
 app.listen(PORT, () => {
