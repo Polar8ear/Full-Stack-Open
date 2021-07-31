1a4cbfaf16855189578819538940fbb645de36cd
Polar8ear <64390556+Polar8ear@users.noreply.github.com>
2021-07-31 08:39:33 +0800

    Set toJSON method for Blog model

--- Part 4/Blogs/models/blog.js
+++ Part 4/Blogs/models/blog.js
@@ -11,6 +11,14 @@ const blogSchema = new mongoose.Schema({
   likes: Number
 })
 
+blogSchema.set('toJSON', {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString()
+    delete returnedObject._id
+    delete returnedObject.__v
+  }
+})
+
 mongoose
   .connect(MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true })  .then(() => logger.info(`Connected to ${MONGODB_URI}`))
   .catch(error => {
