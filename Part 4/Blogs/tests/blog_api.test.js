0a6e925143a7d5ca53d00476cea178a6a82076e7
Polar8ear <64390556+Polar8ear@users.noreply.github.com>
2021-07-31 08:15:52 +0800

    Part 4.8 Created blog_api test

---
+++ Part 4/Blogs/tests/blog_api.test.js
@@ -0,0 +1,32 @@
+const mongoose = require('mongoose')
+const supertest = require('supertest')
+const app = require('../app')
+const Blog = require('../models/blog')
+const helper = require('../utils/test_helper')
+
+
+const api = supertest(app)
+
+beforeEach(async () => {
+  await Blog.deleteMany({})
+
+  const blogObjects = helper.initialNotes
+    .map(blog => new Blog(blog))
+  const promiseArray = blogObjects.map(blog => blog.save())
+  await Promise.all(promiseArray)
+})
+
+describe('When some blogs is saved in DB initially',() => {
+  test('correct number of blogs is returned', async () => {
+    const response = await api
+      .get('/api/blogs')
+      .expect(200)
+  
+    expect(response.body).toHaveLength(helper.initialNotes.length)
+  })
+})
+
+
+afterAll(() => {
+  mongoose.connection.close()
+})
