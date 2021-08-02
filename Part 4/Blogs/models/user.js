c5c990bafea3a033004c7d1e43728c0644aef51c
Polar8ear <64390556+Polar8ear@users.noreply.github.com>
2021-08-02 17:49:31 +0800

    Added blogs field for user model

--- Part 4/Blogs/models/user.js
+++ Part 4/Blogs/models/user.js
@@ -14,7 +14,11 @@ const userSchema = new mongoose.Schema({
   },
   name: {
     type:String
-  }
+  },
+  blogs: [{
+    type:mongoose.Schema.Types.ObjectId,
+    ref:'Blog'
+  }]
 })
 
 userSchema.set('toJSON', {
