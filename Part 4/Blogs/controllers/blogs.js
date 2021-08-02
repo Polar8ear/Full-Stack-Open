f41cd5450c3c7a199a82e683fdbf9754389dfc50
Polar8ear <64390556+Polar8ear@users.noreply.github.com>
2021-08-02 17:48:53 +0800

    Adjust POST to allow user field and populate during GET

--- Part 4/Blogs/controllers/blogs.js
+++ Part 4/Blogs/controllers/blogs.js
@@ -1,16 +1,30 @@
 const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
+const User = require('../models/user')
 
 blogsRouter.get('/', async (request, response) => {
-  const blogs = await Blog.find({})
+  const blogs = await Blog
+    .find({}).populate('user')
   response.json(blogs)
 })
 
 blogsRouter.post('/', async (request, response) => {
-  const blog = new Blog(request.body)
+  const body = request.body
+
+  const user = await User.findOne({})
+  const blog = new Blog({
+    title: body.title,
+    author: body.author,
+    url: body.url,
+    likes: body.likes,
+    user: user._id
+  })
 
   const savedBlog = await blog.save()
   response.status(201).json(savedBlog)
+
+  user.notes = user.notes.concat(savedBlog._id)
+  await user.save()
 })
 
 blogsRouter.delete('/:id', async (request, response) => {
