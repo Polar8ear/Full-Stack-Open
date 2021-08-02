374ae802de63da8526a22698bc8497f4d2b496d7
Polar8ear <64390556+Polar8ear@users.noreply.github.com>
2021-08-02 16:32:41 +0800

    Added validation for username length

--- Part 4/Blogs/models/user.js
+++ Part 4/Blogs/models/user.js
@@ -5,11 +5,12 @@ const userSchema = new mongoose.Schema({
   username: {
     type:String,
     required:true,
-    unique:true
+    minLength:3,
+    unique:true,
   },
   passwordHash: {
     type:String,
-    required:true
+    required:true,
   },
   name: {
     type:String
