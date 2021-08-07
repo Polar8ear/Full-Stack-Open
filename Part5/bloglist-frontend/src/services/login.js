c2859e13a18b8ca5eccc89b1940bad3c1b065efc
Polar8ear <64390556+Polar8ear@users.noreply.github.com>
2021-08-07 21:01:37 +0800

    Created login function

---
+++ Part5/bloglist-frontend/src/services/login.js
@@ -0,0 +1,13 @@
+import axios from 'axios'
+const loginUrl = '/api/login'
+
+const login = async credentials => {
+  const response = await axios.post(loginUrl,credentials)
+  return response.data
+}
+
+const exportObject = {
+  login,
+}
+
+export default exportObject
