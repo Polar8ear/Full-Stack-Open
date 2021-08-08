31e01377ed4d7e8b92fdd821d55bf15fdb8e7fd1
Polar8ear <64390556+Polar8ear@users.noreply.github.com>
2021-08-08 22:42:46 +0800

    Added token functionality

--- Part5/bloglist-frontend/src/services/blogs.js
+++ Part5/bloglist-frontend/src/services/blogs.js
@@ -1,6 +1,16 @@
 import axios from 'axios'
 const baseUrl = '/api/blogs'
 
+let token = null
+
+const setToken = receivedToken => {
+  token = `Bearer ${receivedToken}`
+}
+
+const removeToken = () => {
+  token = null
+}
+
 const getAll = () => {
   const request = axios.get(baseUrl)
   return request.then(response => response.data)
@@ -8,6 +18,8 @@ const getAll = () => {
 
 const exportObject = {
   getAll,
+  setToken,
+  removeToken,
 }
 
 export default exportObject
