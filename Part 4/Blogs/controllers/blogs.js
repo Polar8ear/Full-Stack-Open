731bd0c11c0b5abd24083f018bd3c8c751643157
Polar8ear <64390556+Polar8ear@users.noreply.github.com>
2021-08-14 13:51:28 +0800

    Adjust error handling of put method

--- Part 4/Blogs/controllers/blogs.js
+++ Part 4/Blogs/controllers/blogs.js
@@ -59,12 +59,8 @@ blogsRouter.put('/:id', async (request, response) => {
   const id = request.params.id
   const updatedInfo = { ...request.body }
 
-  await Blog.findByIdAndUpdate(id, updatedInfo, { new:true, runValidators:true },
-    (error,document) => {
-      document!==null
-        ?response.status(200).json(document._doc)
-        :response.status(404).json({ error:`Data with id:${id} is not found` })
-    })
+  const updatedBlog = await Blog.findByIdAndUpdate(id, updatedInfo, { new:true, runValidators:true })
+  response.json(updatedBlog.toJSON())
 })
 
 module.exports = blogsRouter
