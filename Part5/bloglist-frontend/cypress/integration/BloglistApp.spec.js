1d5d8228d58263dc96e9a7fbe78bed0c5390c5af
Polar8ear <64390556+Polar8ear@users.noreply.github.com>
2021-09-08 16:37:39 +0800

    Part 5.17 Test if loginForm shows

---
+++ Part5/bloglist-frontend/cypress/integration/BloglistApp.spec.js
@@ -0,0 +1,11 @@
+describe('Bloglist app', function() {
+  beforeEach(function() {
+    cy.request('POST', 'http://localhost:3003/api/testing/reset')
+    cy.visit('http://localhost:3000')
+  })
+
+  it('Login form is shown', function() {
+    cy.contains('Username').parent().find('input')
+    cy.contains('Password').parent().find('input')
+  })
+})
