c19623bba74ae1bff6084ed909f2d7831aea888d
Polar8ear <ngiubing02@gmail.com>
2021-09-11 23:49:03 +0800

    Part 5.19 Test for creating blog

--- part5/bloglist-frontend/cypress/integration/BloglistApp.spec.js
+++ part5/bloglist-frontend/cypress/integration/BloglistApp.spec.js
@@ -1,22 +1,24 @@
-describe('Bloglist app', function() {
+/* eslint-disable func-names */
+/* eslint-disable prefer-arrow-callback */
+describe('Bloglist app', function () {
   const user = {
     name: 'polar8ear',
-    username : 'polar8ear02',
-    password : 'polar8ear',
+    username: 'polar8ear02',
+    password: 'polar8ear',
   }
-  beforeEach(function() {
+  beforeEach(function () {
     cy.request('POST', 'http://localhost:3003/api/testing/reset')
-    cy.request('POST', 'http://localhost:3003/api/users',user)
+    cy.request('POST', 'http://localhost:3003/api/users', user)
     cy.visit('http://localhost:3000')
   })
 
-  it('Login form is shown', function() {
+  it('Login form is shown', function () {
     cy.contains('Username').parent().find('input')
     cy.contains('Password').parent().find('input')
   })
 
-  describe('Login',function() {
-    it('succeeds with correct credentials', function() {
+  describe('Login', function () {
+    it('succeeds with correct credentials', function () {
       cy.contains('Username').parent().find('input')
         .type(user.username)
 
@@ -32,7 +34,7 @@ describe('Bloglist app', function() {
       cy.contains('Logout')
     })
 
-    it('fails with wrong credentials', function() {
+    it('fails with wrong credentials', function () {
       cy.contains('Username').parent().find('input')
         .type(user.username)
 
@@ -43,9 +45,43 @@ describe('Bloglist app', function() {
         .click()
 
       cy.get('.notification.error')
-        .should('contain','Invalid username or password')
+        .should('contain', 'Invalid username or password')
         .and('have.css', 'color', 'rgb(255, 0, 0)')
         .and('have.css', 'border-style', 'solid')
+    })
+  })
+
+  describe('when logged in', function () {
+    const newBlog = {
+      title: 'Amazon starts making its own TVs with new Fire TV Omni and 4-Series',
+      author: 'Chris Welch',
+      url: 'https://www.theverge.com/2021/9/9/22662673/amazon-fire-tv-omni-4-series-price-features',
+    }
+
+    beforeEach(function () {
+      cy.request('POST', 'http://localhost:3003/api/login', { username: user.username, password: user.password })
+        .then((response) => {
+          localStorage.setItem('user', JSON.stringify(response.body))
+          cy.visit('http://localhost:3000')
+        })
+    })
+
+    it('a blog can be created', function () {
+      cy.contains('Create New Blog')
+        .click()
+
+      cy.get('#title').type(newBlog.title)
+      cy.get('#author').type(newBlog.author)
+      cy.get('#url').type(newBlog.url)
+
+      cy.get('#createBtn').click()
+
+      cy.get('.notification')
+        .should('contain', 'a new blog')
+        .and('contain', 'added')
+        .and('have.css', 'color', 'rgb(0, 128, 0)')
+
+      cy.get('#blogs').contains(`${newBlog.title} ${newBlog.author}`)
     })
   })
 })
