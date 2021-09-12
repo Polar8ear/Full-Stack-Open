d7ca999a131786ae899ac3f95ee6326cd54e19fe
Polar8ear <ngiubing02@gmail.com>
2021-09-12 16:17:31 +0800

    Part 5.20 Test for liking blogs

--- part5/bloglist-frontend/cypress/integration/BloglistApp.spec.js
+++ part5/bloglist-frontend/cypress/integration/BloglistApp.spec.js
@@ -47,7 +47,6 @@ describe('Bloglist app', function () {
       cy.get('.notification.error')
         .should('contain', 'Invalid username or password')
         .and('have.css', 'color', 'rgb(255, 0, 0)')
-        .and('have.css', 'border-style', 'solid')
     })
   })
 
@@ -58,12 +57,37 @@ describe('Bloglist app', function () {
       url: 'https://www.theverge.com/2021/9/9/22662673/amazon-fire-tv-omni-4-series-price-features',
     }
 
+    const initialBlogs = [
+      {
+        title: 'Facebook, Ray-Ban debut picture-taking smart glasses',
+        author: 'Ina Fried',
+        url: 'https://www.axios.com/facebook-ray-ban-wayfarer-stories-5f113bfa-797f-4e2d-b99c-7d65a8726607.html',
+      },
+      {
+        title: 'BioNTech mRNA Cancer Treatment Moved To Human Trials After Huge Success In Mice',
+        author: 'Jack Dunhill',
+        url: 'https://www.iflscience.com/health-and-medicine/biontech-mrna-cancer-treatment-moved-to-human-trials-after-huge-success-in-mice',
+      },
+    ]
+
     beforeEach(function () {
       cy.request('POST', 'http://localhost:3003/api/login', { username: user.username, password: user.password })
         .then((response) => {
-          localStorage.setItem('user', JSON.stringify(response.body))
-          cy.visit('http://localhost:3000')
+          const userInfo = response.body
+          localStorage.setItem('user', JSON.stringify(userInfo))
+
+          initialBlogs.forEach((blog) => {
+            const options = {
+              method: 'POST',
+              url: 'http://localhost:3003/api/blogs',
+              headers: { Authorization: `Bearer ${userInfo.token}` },
+              body: blog,
+            }
+            cy.request(options)
+          })
         })
+
+      cy.visit('http://localhost:3000')
     })
 
     it('a blog can be created', function () {
@@ -82,6 +106,19 @@ describe('Bloglist app', function () {
         .and('have.css', 'color', 'rgb(0, 128, 0)')
 
       cy.get('#blogs').contains(`${newBlog.title} ${newBlog.author}`)
+    })
+
+    it('a blog can be liked', function () {
+      const blogToBeLiked = initialBlogs[0]
+      cy.get('#blogs')
+        .contains(blogToBeLiked.title)
+        .within(function () {
+          cy.get('.viewBtn').click()
+          cy.contains('Likes:0')
+
+          cy.get('.likeBtn').click()
+          cy.contains('Likes:1')
+        })
     })
   })
 })
