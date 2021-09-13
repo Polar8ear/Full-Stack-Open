ed75f4710437be79fcc7096ae46235cee2fee5a8
Polar8ear <ngiubing02@gmail.com>
2021-09-14 00:39:07 +0800

    Add command to clean up the code

--- part5/bloglist-frontend/cypress/support/commands.js
+++ part5/bloglist-frontend/cypress/support/commands.js
@@ -1,3 +1,5 @@
+/* eslint-disable func-names */
+/* eslint-disable prefer-arrow-callback */
 // ***********************************************
 // This example commands.js shows you how to
 // create various custom commands and overwrite
@@ -11,18 +13,6 @@
 //
 // -- This is a parent command --
 // Cypress.Commands.add('login', (email, password) => { ... })
-Cypress.Commands.add('login', (user) => {
-  const loginURL = 'http://localhost:3003/api/login'
-  cy.request({
-    url: loginURL,
-    method: 'POST',
-    body: user,
-  })
-    .then((response) => {
-      localStorage.setItem('user', JSON.stringify(response.body))
-      cy.wrap(response.body.token).as('token')
-    })
-})
 //
 //
 // -- This is a child command --
@@ -35,3 +25,32 @@ Cypress.Commands.add('login', (user) => {
 //
 // -- This will overwrite an existing command --
 // Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+
+Cypress.Commands.add('login', (user) => {
+  const loginURL = 'http://localhost:3003/api/login'
+  cy.request({
+    url: loginURL,
+    method: 'POST',
+    body: user,
+  })
+    .then((response) => {
+      localStorage.setItem('user', JSON.stringify(response.body))
+      cy.wrap(response.body.token).as('token')
+    })
+})
+
+Cypress.Commands.add('likeBlogOf', (blogToBeLiked) => {
+  cy.get('#blogs')
+    .contains(blogToBeLiked.title)
+    .within(function () {
+      cy.get('.likeBtn').click()
+    })
+})
+
+Cypress.Commands.add('viewBlogOf', (blogToBeViewed) => {
+  cy.get('#blogs')
+    .contains(blogToBeViewed.title)
+    .within(function () {
+      cy.get('.viewBtn').click()
+    })
+})
