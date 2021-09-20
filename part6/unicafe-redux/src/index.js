e2fb3bb51bfb3eb549576213c67ad9d913105a74
Polar8ear <64390556+Polar8ear@users.noreply.github.com>
2021-09-21 00:36:29 +0800

    Added type for button and remove semicolon

--- part6/unicafe-redux/src/index.js
+++ part6/unicafe-redux/src/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React from 'react'
 import ReactDOM from 'react-dom'
 import { createStore } from 'redux'
 import reducer from './reducer'
@@ -8,17 +8,20 @@ const store = createStore(reducer)
 const App = () => {
   const good = () => {
     store.dispatch({
-      type: 'GOOD'
+      type: 'GOOD',
     })
   }
 
   return (
     <div>
-      <button onClick={good}>good</button> 
-      <button>neutral</button> 
-      <button>bad</button>
-      <button>reset stats</button>
-      <div>good {store.getState().good}</div>
+      <button type="button" onClick={good}>good</button>
+      <button type="button">neutral</button>
+      <button type="button">bad</button>
+      <button type="button">reset stats</button>
+      <div>
+        good
+        {store.getState().good}
+      </div>
       <div>neutral</div>
       <div>bad</div>
     </div>
