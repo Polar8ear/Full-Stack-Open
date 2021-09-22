c5aa2ddb047fd5d585fafd7df07e1b649c25c6fb
Polar8ear <64390556+Polar8ear@users.noreply.github.com>
2021-09-22 13:02:45 +0800

    fix: Access anecdotes props of state since using combined reducers

--- part6/redux-anecdotes/src/components/AnecdoteList.js
+++ part6/redux-anecdotes/src/components/AnecdoteList.js
@@ -4,7 +4,7 @@ import { useDispatch, useSelector } from 'react-redux'
 import { voteAnecdote } from '../reducers/anecdoteReducer'
 
 const AnecdoteList = () => {
-  const anecdotes = useSelector(state => state)
+  const anecdotes = useSelector(state => state.anecdotes)
   const sortedAnecdotes = anecdotes.sort((a, b) => b.votes - a.votes)
 
   const dispatch = useDispatch()
