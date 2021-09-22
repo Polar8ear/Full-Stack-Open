3d9d485e0a0a136e8b43f717a07a2056b9966a4f
Polar8ear <64390556+Polar8ear@users.noreply.github.com>
2021-09-22 15:32:00 +0800

    feat: Added action creators for creating and removing notification

--- part6/redux-anecdotes/src/reducers/notificationReducer.js
+++ part6/redux-anecdotes/src/reducers/notificationReducer.js
@@ -1,15 +1,29 @@
-const initialState = {
-  message: 'OLAAAAA',
-}
+const initialState = {}
 
 const reducer = (state = initialState, action) => {
   switch(action.type){
     case('NEW_NOTIFICATION'):
       return {...action.data}
 
+    case('REMOVE_NOTIFICATION'):
+      return initialState
+
     default:
       return state
   }
+}
+
+export const createNewNotification = (message) => {
+  return({
+    type: 'NEW_NOTIFICATION',
+    data: { message }
+  })
+}
+
+export const removeNotification = () => {
+  return({
+    type: 'REMOVE_NOTIFICATION'
+  })
 }
 
 export default reducer
