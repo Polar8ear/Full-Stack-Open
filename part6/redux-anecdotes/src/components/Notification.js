c3a7002941c12822bd770e3a8d3e9670180b93fd
Polar8ear <64390556+Polar8ear@users.noreply.github.com>
2021-09-22 15:30:56 +0800

    feat: display none when no message

--- part6/redux-anecdotes/src/components/Notification.js
+++ part6/redux-anecdotes/src/components/Notification.js
@@ -4,14 +4,17 @@ import { useSelector } from 'react-redux'
 
 const Notification = () => {
   const notification = useSelector((state) => state.notification)
+  const message = notification.message
+
   const style = {
     border: 'solid',
     padding: 10,
-    borderWidth: 1
+    borderWidth: 1,
+    display: message ? 'block' : 'none'
   }
   return (
     <div style={style}>
-      {notification.message}
+      {message}
     </div>
   )
 }
