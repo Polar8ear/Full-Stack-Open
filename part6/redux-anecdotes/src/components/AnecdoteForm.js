84283c57d577412984392afe37f2b50e157edb27
Polar8ear <64390556+Polar8ear@users.noreply.github.com>
2021-09-23 19:58:08 +0800

    feat: Part 6.16 Apllied async action creator for create anecdote

--- part6/redux-anecdotes/src/components/AnecdoteForm.js
+++ part6/redux-anecdotes/src/components/AnecdoteForm.js
@@ -3,7 +3,6 @@ import React from 'react'
 import { useDispatch } from 'react-redux'
 import { createNewAnecdote } from '../reducers/anecdoteReducer'
 import { popNotification } from '../helpers/anectodeHelpers'
-import anecdoteService from '../services/anecdote'
 
 const AnecdoteForm = () => {
   const dispatch = useDispatch()
@@ -13,8 +12,7 @@ const AnecdoteForm = () => {
     const anecdoteContent = event.target.content.value
     event.target.content.value = ''
 
-    const newAnecdote = await anecdoteService.createNew(anecdoteContent)
-    dispatch(createNewAnecdote(newAnecdote))
+    dispatch(createNewAnecdote(anecdoteContent))
     popNotification(
       'An anectode has been added',
       dispatch,
