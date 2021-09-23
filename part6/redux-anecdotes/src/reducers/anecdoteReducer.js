0b3371d36dc75aab37e7528846eecc7a084f750b
Polar8ear <64390556+Polar8ear@users.noreply.github.com>
2021-09-23 15:21:14 +0800

    fix: Object structure when receiving new anecdote

--- part6/redux-anecdotes/src/reducers/anecdoteReducer.js
+++ part6/redux-anecdotes/src/reducers/anecdoteReducer.js
@@ -1,23 +1,4 @@
-const anecdotesAtStart = [
-  'If it hurts, do it more often',
-  'Adding manpower to a late software project makes it later!',
-  'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
-  'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
-  'Premature optimization is the root of all evil.',
-  'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.'
-]
-
-const getId = () => (100000 * Math.random()).toFixed(0)
-
-const asObject = (anecdote) => {
-  return {
-    content: anecdote,
-    id: getId(),
-    votes: 0
-  }
-}
-
-const initialState = anecdotesAtStart.map(asObject)
+const initialState = []
 
 const reducer = (state = initialState, action) => {
   switch(action.type){
@@ -28,7 +9,7 @@ const reducer = (state = initialState, action) => {
       return state.map((anecdote) => anecdote.id===id?updatedNote:anecdote)
 
     case('NEW_ANECDOTE'):
-      const newAnecdote = asObject(action.data.content)
+      const newAnecdote = action.data
       return state.concat(newAnecdote)
 
     case('INIT_ANECDOTES'):
@@ -40,10 +21,10 @@ const reducer = (state = initialState, action) => {
 }
 
 //Below are action creators
-export const createNewAnecdote = (content) => {
+export const createNewAnecdote = (payload) => {
   return({
     type: 'NEW_ANECDOTE',
-    data: { content }
+    data: { ...payload }
   })
 }
 
