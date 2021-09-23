190bd8fbb54d4f858f7e67a21dacee6ff4b1b889
Polar8ear <64390556+Polar8ear@users.noreply.github.com>
2021-09-23 23:08:19 +0800

    refactor: Replaced by notification action creator

--- part6/redux-anecdotes/src/helpers/anectodeHelpers.js
+++
@@ -1,9 +0,0 @@
-import { createNewNotification, removeNotification } from "../reducers/notificationReducer"
-
-export const popNotification = (message,dispatch) => {
-  const durationInms = 5000
-  dispatch(createNewNotification(message))
-  setTimeout(()=>{
-    dispatch(removeNotification())
-  }, durationInms)
-}
