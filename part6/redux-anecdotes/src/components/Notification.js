33cf3be7ab3a775fd38c814e8fe7c2716bb1db7a
Polar8ear <64390556+Polar8ear@users.noreply.github.com>
2021-09-24 12:08:35 +0800

    feat: Part 6.19 Replace useSelector with redux connect

--- part6/redux-anecdotes/src/components/Notification.js
+++ part6/redux-anecdotes/src/components/Notification.js
@@ -1,9 +1,9 @@
 
 import React from 'react'
-import { useSelector } from 'react-redux'
+import { connect } from 'react-redux'
 
-const Notification = () => {
-  const notification = useSelector((state) => state.notification)
+const Notification = (props) => {
+  const notification = props.notification
   const message = notification.message
 
   const style = {
@@ -19,4 +19,12 @@ const Notification = () => {
   )
 }
 
-export default Notification
+const mapStateToProps = (state) => {
+  return({
+    notification: state.notification
+  })
+}
+
+const connectedNotification = connect(mapStateToProps)(Notification)
+
+export default connectedNotification
