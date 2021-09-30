780f884fc8f97d1930ce3928ce87cf16bc95e0ab
Polar8ear <64390556+Polar8ear@users.noreply.github.com>
2021-09-30 13:53:05 +0800

    feat: Part 7.8 Implement initialize and create service for useResource hook

--- part7/ultimate-hooks/src/App.js
+++ part7/ultimate-hooks/src/App.js
@@ -20,14 +20,21 @@ const useField = (type) => {
 const useResource = (baseUrl) => {
   const [resources, setResources] = useState([])
 
-  // ...
+  const initialize = async () => {
+    const response = await axios.get(baseUrl)
+    setResources(response.data)
+  }
 
-  const create = (resource) => {
-    // ...
+
+  const create = async (resource) => {
+    const response = await axios.post(baseUrl, resource)
+    const newResource = response.data
+    setResources(resources.concat(newResource))
   }
 
   const service = {
-    create
+    initialize,
+    create,
   }
 
   return [
