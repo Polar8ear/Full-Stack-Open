e7e21b02d3043a32a2da4ff3034e64c602a0fe0a
Polar8ear <64390556+Polar8ear@users.noreply.github.com>
2022-02-10 21:40:46 +0800

    fix: ? operator and arrow function return

--- part7/bloglist-frontend/src/reducers/userReducer.js
+++ part7/bloglist-frontend/src/reducers/userReducer.js
@@ -1,29 +1,25 @@
 const userReducer = (state = {}, action) => {
   switch (action.type) {
   case 'CHANGE_USERNAME':
-    return { ...state, username: action.data?username }
+    return { ...state, username: action.data.username }
 
   case 'CHANGE_PASSWORD':
-    return { ...state, password: action.data?password }
+    return { ...state, password: action.data.password }
 
   default:
     return state
   }
 }
 
-const changeUsername = (username) => {
-    return {
-        type: 'CHANGE_USERNAME',
-        username,
-    }
-}
+const changeUsername = (username) => ({
+  type: 'CHANGE_USERNAME',
+  username,
+})
 
-const changePassword = (password) => {
-    return {
-        type: 'CHANGE_PASSWORD',
-        password,
-    }
-}
+const changePassword = (password) => ({
+  type: 'CHANGE_PASSWORD',
+  password,
+})
 
 export { changeUsername, changePassword }
 
