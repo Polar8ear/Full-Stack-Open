64bd85679c267ed8404a5d24d502f36278bd0696
Polar8ear <64390556+Polar8ear@users.noreply.github.com>
2022-02-17 00:23:04 +0800

    refactor: Remove actions for loginForm

--- part7/bloglist-frontend/src/reducers/userReducer.js
+++ part7/bloglist-frontend/src/reducers/userReducer.js
@@ -1,30 +1,9 @@
 const userReducer = (state = {}, action) => {
   switch (action.type) {
-    case "CHANGE_USERNAME":
-      return { ...state, username: action.data.username }
-
-    case "CHANGE_PASSWORD":
-      return { ...state, password: action.data.password }
 
     default:
       return state
   }
 }
-
-const changeUsername = (username) => ({
-  type: "CHANGE_USERNAME",
-  data: {
-    username,
-  },
-})
-
-const changePassword = (password) => ({
-  type: "CHANGE_PASSWORD",
-  data: {
-    password,
-  },
-})
-
-export { changeUsername, changePassword }
 
 export default userReducer
