75d7d7841e9cc80e19a13234acf15ca9469ecef2
Polar8ear <64390556+Polar8ear@users.noreply.github.com>
2022-02-22 11:57:04 +0800

    feat: Created page for each blog view

---
+++ part7/bloglist-frontend/src/pages/Blog.js
@@ -0,0 +1,38 @@
+import React from "react"
+import { useDispatch, useSelector } from "react-redux"
+import { useParams } from "react-router-dom"
+import { likeBlog } from "../reducers/blogsReducer"
+
+const User = () => {
+  const dispatch = useDispatch()
+  const params = useParams()
+  const blogs = useSelector((state) => state.blogs)
+
+  const id = params.blogId
+  const selectedBlog = blogs.find((user) => user.id === id)
+
+  const handleLikeBlog = () => {
+    dispatch(likeBlog(id))
+  }
+
+  if (!selectedBlog) {
+    return null
+  }
+
+  return (
+    <div>
+      <h2>{selectedBlog.name}</h2>
+      <h2>{selectedBlog.author}</h2>
+      <a href={selectedBlog.url}>{selectedBlog.url}</a>
+      <div>
+        {selectedBlog.likes} likes
+        <button type="button" onClick={() => handleLikeBlog(selectedBlog.id)}>
+          Like
+        </button>
+      </div>
+      <p>Added by {selectedBlog.user.name}</p>
+    </div>
+  )
+}
+
+export default User
